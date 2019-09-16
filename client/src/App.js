893b9a259e356bb4d6521b13137828e04c80c224
Ayomide Jones <ayormeday@gmail.com>
2019-09-16 15:07:47 +0100

    added functionality to the Home button

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom'
-import SavedList from './Movies/SavedList';
-import Movie from './Movies/Movie';
-import MovieList from './Movies/MovieList';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import SavedList from "./Movies/SavedList";
+import Movie from "./Movies/Movie";
+import MovieList from "./Movies/MovieList";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList} />
-      <Route exact path="/movies/:id" component={Movie} />
-      <div>Replace this Div with your Routes</div>
+      <Route
+        path="/movies/:id"
+        render={props => <Movie {...props} addToSavedList={addToSavedList} />}
+      />
     </div>
   );
 };
